b8a6c550bbae60235f248073f0eb4e359b7b15e2
Jefferson Gibin <jefferson.gibin@kabum.com.br>
2019-12-13 15:04:47 -0300

    remoção de comentários

--- src/routes/usuario-route.js
+++ src/routes/usuario-route.js
@@ -1,23 +1,22 @@
 const { check, validationResult } = require('express-validator');
 const { Usuario } = require('../controllers')
 
+/**
+ * Router Usuário
+ */
 module.exports = (app) => {
-
-	// usuarios
 	app.get('/v1/usuario', (req, res) => {
 		Usuario.getAll(req, res)
 			.then((response) => res.status(200).send(response))
 			.catch((e) => res.status(500).json({ status: false }))
 	});
 
-	// delete
 	app.get('/v1/usuario/:id', (req, res) => {
 		Usuario.getByID(req.params.id)
 			.then((response) => res.status(200).send(response))
 			.catch((e) => res.status(500).json({ status: false }))
 	});
 
-	// salvar
 	app.post('/v1/usuario', [
 		check('nome').isString(),
 		check('senha').isString(),
@@ -36,7 +35,6 @@ module.exports = (app) => {
 
 	});
 
-	// atualizar
 	app.put('/v1/usuario/:id', [
 		check('nome').isString(),
 		check('senha').isString(),
@@ -56,7 +54,6 @@ module.exports = (app) => {
 			})
 	});
 
-	// delete
 	app.delete('/v1/usuario/:id', (req, res) => {
 		Usuario.delete(req.params.id)
 			.then((response) => res.status(200).send(response))
