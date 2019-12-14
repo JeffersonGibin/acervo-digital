c57eeecdee32896735516c00a55376413b0cd9fe
Jefferson <jeffersongibin@yahoo.com.br>
2019-12-14 03:05:31 -0300

    Adicionado comentarios no app.js

--- src/app.js
+++ src/app.js
@@ -5,20 +5,34 @@ const app = express()
 const bindParam = require('./middlewares/bindParam')
 const cors = require('cors')
 
+/**
+ * Inicia as configurações do DOTENV
+ */
 dotenv.config()
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+/**
+ * Middlewares
+ */
 app.use(bindParam)
 
 /**
- * Boa pratica para invasores não identificar aplicação executando o Express 
+ * Boa prática para invasores não identificar aplicações executando o Express 
 */
 app.disable('x-powered-by');
 
+/**
+ * Rota
+ * Importa todas as rotas.
+*/
 require('./routes/index.js')(app)
 
+/**
+ * Tratamento para rotas que não existem.
+ */
 app.use(function(req, res, next) {
     res.status(404).send({
         status: false,
