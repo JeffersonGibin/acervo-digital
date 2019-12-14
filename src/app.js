af8b88ff47918360416c462e0df18af68daa5404
Jefferson <jeffersongibin@yahoo.com.br>
2019-12-14 03:01:36 -0300

    Adicionado tratamento cors no projeto

--- src/app.js
+++ src/app.js
@@ -3,12 +3,19 @@ const bodyParser  = require('body-parser')
 const dotenv = require('dotenv')
 const app = express()
 const bindParam = require('./middlewares/bindParam')
+const cors = require('cors')
 
 dotenv.config()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bindParam)
+
+/**
+ * Boa pratica para invasores não identificar aplicação executando o Express 
+*/
+app.disable('x-powered-by');
 
 require('./routes/index.js')(app)
 
